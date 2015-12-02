a237f76e0b7701ebbac4d90bce4a6cddaba9619b
mdeverhansen <mdeverhansen@email.arizona.edu>
2015-12-02 15:19:53 -0700

    addMoviePage and loginPage started

--- addMoviePage.php
+++ addMoviePage.php
@@ -16,28 +16,57 @@
 	<header>
         <img src="MoviesToUse/images/rancidbanner.png" alt="Rancid Tomatoes">
         <!-- may want to make this a link to the home page -->
+        <!--    this needs to extend across the window -->
     </header>
     <div class="container">
     	<div class="row main-header">
     		<h1>Add a New Movie</h1>
     	</div>
     	<div class="main row">
-    		<form action="addMovie.php" method="post"><br />
-    			Title: <br /> <input type="text" name="title" /><br />
-    			Year: <br /> <input type="date" name="year" /><br />
-    			My Rating: <br /> <input type="text" name="rating" /><br />
-    			Cast: <br /> <input type="text" name="cast" /><br />
-    			Director: <br /> <input type="text" name="director" /><br />
-    			Producer: <br /> <input type="text" name="producer" /><br />
-    			MPAA Rating: <br /> <input type="text" name="mpaaRating" /><br />
-    			Release Date: <br /> <input type="date" name="releaseDate" /><br />
-    			Synopsis: <br /> <textarea name="synopsis" rows="10" cols="50"></textarea><br />
-    			Production Company: <br /> <input type="text" name="productionCompany" /><br />
-    			Run Time (in minutes): <br /> <input type="number" name="runTime" /><br />
-    			Genre: <br /> <input type="text" name="genre" /><br />
-    			Box Office: <br /> <input type="number" name="title" /><br />
-    			links: <br /> <input type="text" name="links" /><br />
-    		</form>
+            <section class="reviews-container">
+                <aside class="ratings-container">
+                    <img src="MoviesToUse/images/rottenlarge.png" alt="Rotten" /> 
+                    <span class="addMovieInstr">Enter Movie Info Below:</span>
+                    <img class="addMovieFresh" src="MoviesToUse/images/freshlarge.png" alt="Fresh" />
+                </aside>
+        		<form action="addMovie.php" method="post"><br />
+                    <!-- want to check title against database as typed? -->
+        			Title: <br /> <input type="text" name="title" /><br />
+        			Year: <br /> <input type="date" name="year" /><br />
+        			My Rating: <br /> <input type="text" name="rating" /><br />
+        			Cast: <br /> <input type="text" name="cast" /><br />
+        			Director: <br /> <input type="text" name="director" /><br />
+        			Producer: <br /> <input type="text" name="producer" /><br />
+        			MPAA Rating: <br /> <input type="radio" name="mpaaRating" value="G"/>G
+                                        <input type="radio" name="mpaaRating" value="PG" />PG
+                                        <input type="radio" name="mpaaRating" value="PG-13"/>PG-13
+                                        <input type="radio" name="mpaaRating" value="R" />R
+                                        <!-- do we want this one?
+                                        <input type="radio" name="mpaaRating" value="NC-17" /> -->
+                    <br />
+        			Release Date: <br /> <input type="date" name="releaseDate" /><br />
+        			Synopsis: <br /> <textarea name="synopsis" rows="10" cols="50"></textarea><br />
+        			Production Company: <br /> <input type="text" name="productionCompany" /><br />
+        			Run Time (in minutes): <br /> <input type="number" name="runTime" /><br />
+        			Genre: <br /> <input type="text" name="genre" /><br />
+        			Box Office: <br /> <input type="number" name="title" /><br />
+                    <!--want to actually make these working links? if yes, check security? -->
+        			links: <br /> <input type="text" name="links" /><br />
+                    <br />
+                    <button type="button" value="clear" onclick="SOMETHING">Clear</button>
+                    <br /><br />
+                    <input type="submit" value="submit" />
+        		</form>
+            </section>
+            <section class="overview">
+                <a rel="url" href="randcidHome.php">Home</a>
+                <br />
+                <!-- put this as an if not signed in, make visible? -->
+                <a rel="url"  href="loginPage.php">Sign In</a><!-- do this as a link or not? -->
+            </section>
+            <footer class="row">
+                <!-- it'd be cool to have a "top" link here -->
+            </footer>
     	</div>
     </div>
 </body>
---
+++ loginPage.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+/*require_once ('SOMETHING.PHP');*/
+	/*set up variables we may need here*/
+?>
+
+<head>
+	<title>RT: Add a Movie</title>
+	<meta charset="utf-8" />
+	<link href="movie.css" type="text.css" rel="stylesheet" />
+</head>
+
+<body>
+	<header>
+        <img src="MoviesToUse/images/rancidbanner.png" alt="Rancid Tomatoes" />
+        <!-- may want to make this a link to the home page -->
+        <!--    this needs to extend across the window, not sure why it's not -->
+    </header>
+    <div class="container">
+    	<div class="main row">
+	    	<section class="reviews-container">
+	    		<aside class="ratings-container">
+	                    <img src="MoviesToUse/images/rottenlarge.png" alt="Rotten" /> 
+	                    <span class="addMovieInstr">Login and Membership</span>
+	                    <img class="addMovieFresh" src="MoviesToUse/images/freshlarge.png" alt="Fresh" />
+	            </aside>
+	            <div class="loginBoxBig">
+	            	<h2 class="loginHeader">I am already a member!</h2>
+	            	<div class="loginBox">
+		            	My login info:
+		            	<form action="memberLogin.php" method="post"><br />
+		            		<input type="hidden" name="loginType" value="memberLogin" />
+		            		User name: <br /> <input type="text" name="memberUserName" /><br />
+		            		Password: <br /> <input type="password" name="memberPassword" /><br />
+		            		<input type="submit" value="submit" />
+		            	</form>
+		            </div>
+	            </div>
+	            <br /><br /><br />
+		        <div class="loginBoxBig">
+		            <h2 class="loginHeader">I want to become a member!</h2>
+		            <div class="loginBox">
+		            	Enter your info:
+		            	<form action="newLogin.php" method="post"><br />
+		            		<input type="hidden" name="loginType" value="newMember"/>
+		            		Your name: <br /> <input type="text" name="newMemberName" />
+		            		Choose a user name: <br /> <input type="text" name="newMemberUserName" /><br />
+		            		Password: <br /> <input type="password" name="newMemberPassword1" /><br />
+		            		Retype Password: <br /> <input type="password" name="newMemberPassword2" /><br />
+		            		<input type="submit" value="submit" />
+		            	</form>
+		            </div>
+	            </div>
+	    	</section>
+	    	<section class="overview">
+	    		Home <br />
+	    		Add Movie
+	    	</section>
+	    </div>
+    </div>
+</body>
+
+</html>
