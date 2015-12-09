47bb2ee703c74ccf49b6846d2c3194b1cfdbdf28
mdeverhansen <mdeverhansen@email.arizona.edu>
2015-12-09 09:55:48 -0700

    inline minor change movie.php

--- Done/movie.php
+++ Done/movie.php
@@ -45,21 +45,23 @@ require_once("./model.php");
         <div class="row main-header">
             <!-- The php code prints the movie title and year it was made -->
             <h1><?=$film?> (<?=$year?>)</h1>
-        </div>
+        
         <div class="linkBox">
-            <?php if(!isset($_SESSION['user'])) { ?>
-                <a rel="url" href="login.php">Login/Register</a>
-            <?php }else { ?>
-                <a rel="url" href="addReview.php">Add a Review</a>
-                <a rel="url" href="addMovie.php">Add a Movie</a>
-                
-                <a rel="url" href="logout.php">Logout</a>
-            <?php } ?>
-        </div>
-        <form id="searchForm" action="movie.php" method="post">
+            <form id="searchForm" action="movie.php" method="post">
+                <?php if(!isset($_SESSION['user'])) { ?>
+                    <a rel="url" href="login.php">Login/Register</a>
+                <?php }else { ?>
+                    <a rel="url" href="addReview.php">Add a Review</a>
+                    <a rel="url" href="addMovie.php">Add a Movie</a>
+                    
+                    <a rel="url" href="logout.php">Logout</a>
+                <?php } ?>
+        
+            
                     <input type="search" id="searchText" name="searchText" oninput="findMovies()">
                     <input type="submit" id="movieSearch" name="movieSearch" value="Search">
-                </form>
+            </form>
+        </div>
         <div class="main row">
             <section class="reviews-container">
                 <aside class="ratings-container">
